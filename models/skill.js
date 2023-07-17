949e632999a43073e2645cdedd5505b5b4103ca4
drejmin <djjamn@gmail.com>
2023-07-16 20:58:31 -0700

    new file:   controllers/skills.js 	new file:   data/skills.js 	new file:   models/skill.js 	modified:   public/stylesheets/style.css 	modified:   views/index.ejs 	new file:   views/show.ejs 	new file:   views/skills.js

---
+++ models/skill.js
@@ -0,0 +1,19 @@
+const skills = [
+    {skill: 'javascript', progress: true},
+    {skill: 'css', progress: true},
+    {skill: 'html', progress: true},
+    {skill: 'iteration', progress: true},
+    {skill: 'functions', progress: true},
+    {skill: 'arrays', progress: true},
+    {skill: 'mongodb', progress: true},
+    {skill: 'mongoose', progress: true},
+    {skill: 'oauth', progress: true},
+  ];
+
+  module.exports = {
+    getAll,
+  };
+
+  function getAll(){
+    return skills;
+}
