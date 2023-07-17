856038c1f836ceb0d4930541b1d8ee6b4c13195b
drejmin <djjamn@gmail.com>
2023-07-16 23:51:17 -0700

    modified:   models/skill.js 	modified:   controllers/skills.js 	modified:   routes/skills.js 	views/skills/new.ejs

--- models/skill.js
+++ models/skill.js
@@ -12,8 +12,31 @@ const skills = [
 
   module.exports = {
     getAll,
+    create,
+    update,
+    deleteOne,
   };
 
+
+function update(id, updatedSkill) {
+    id = parseInt(id);
+    const skill = skills.find(skill => skill.id === id);
+    Object.assign(skill, updatedSkill);
+  }
+  
+  function deleteOne(id) {
+    id = parseInt(id);
+    // Find the index for the todo
+    const idx = skills.findIndex(skill => skill.id === id);
+    skills.splice(idx, 1);
+  }
+  
+  function create(skill) {
+    // Add the id
+    skill.id = Date.now() % 1000000;
+    skill.done = false;
+    skills.push(skill);
+  }
   function getAll(){
     return skills;
 }
